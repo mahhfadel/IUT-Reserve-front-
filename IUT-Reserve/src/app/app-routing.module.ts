2b078399168ff4f0f03eff6ad9eaf3d24b54a4bb
Maria Clara Fadel <mariaclarapereira@alunos.utfpr.edu.br>
2022-12-07 23:18:46 -0300

    commir routing vinicius

--- IUT-Reserve/src/app/app-routing.module.ts
+++ IUT-Reserve/src/app/app-routing.module.ts
@@ -5,12 +5,18 @@ import { LoginComponent } from './pages/login/login.component';
 import { RegistrationComponent } from './pages/registration/registration.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ReservasAdmComponent } from './pages/reservas-adm/reservas-adm.component';
+import { AdmUserComponent } from './pages/adm-user/adm-user.component';
+import { AdmperfilComponent } from './pages/admperfil/admperfil.component';
+import { UserperfilComponent } from './pages/userperfil/userperfil.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'registration', component: RegistrationComponent },
   { path: 'home', component: HomeComponent },
   { path: 'reservas-adm', component: ReservasAdmComponent },
+  { path: 'adm-user', component: AdmUserComponent },
+  { path: 'admperfil', component: AdmperfilComponent },
+  { path: 'userperfil', component: UserperfilComponent },
 ];
 
 @NgModule({
