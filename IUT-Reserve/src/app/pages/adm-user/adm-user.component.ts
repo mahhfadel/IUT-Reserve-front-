af301316bfe9a3cf05d4de82a4d1011328cb0e07
Maria Clara Fadel <mariaclarapereira@alunos.utfpr.edu.br>
2022-12-08 06:02:54 -0300

    criado adm-user

--- IUT-Reserve/src/app/pages/adm-user/adm-user.component.ts
+++ IUT-Reserve/src/app/pages/adm-user/adm-user.component.ts
@@ -6,6 +6,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./adm-user.component.scss'],
 })
 export class AdmUserComponent implements OnInit {
+  //modal sair
+  menu: boolean = false;
+
+  toggleMenu() {
+    this.menu = !this.menu;
+  }
+
+  //modal sair
+  mostrar: boolean = false;
+
+  toggle() {
+    this.mostrar = !this.mostrar;
+  }
+
+  //modal aprovar
+  aprovar: boolean = false;
+
+  toggleAprovar() {
+    this.aprovar = !this.aprovar;
+  }
+
+  //modal reprovar
+  reprovar: boolean = false;
+
+  toggleReprovar() {
+    this.reprovar = !this.reprovar;
+  }
+
+  //modal excluir
+  excluir: boolean = false;
+
+  toggleExcluir() {
+    this.excluir = !this.excluir;
+  }
+
+  //modal more pendentes
+  pendentes: boolean = false;
+
+  togglePendentes() {
+    this.pendentes = !this.pendentes;
+  }
+
+  //modal more aprovados
+  aprovados: boolean = false;
+
+  toggleAprovados() {
+    this.aprovados = !this.aprovados;
+  }
+
   constructor() {}
 
   ngOnInit(): void {}
